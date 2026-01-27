8287695e1c2eba265f3e673ad71e7c5665fcc2d8
Karthik <karthikreddypallam@gmail.com>
2026-01-27 08:55:56 +0530

    added the onchange events

--- shoppinglist/src/components/Form.tsx
+++ shoppinglist/src/components/Form.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 function Form() {
+  const [product, setProduct] = useState<string>("");
+  const [quantity, setQuantity] = useState<number>(1);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (product) {
+      const newTodo = {
+        id: Date.now(),
+        packed: false,
+        product,
+        quantity,
+      };
+      console.log(newTodo);
+    }
+  };
   return (
-    <form>
-      <select>
+    <form className="form" onSubmit={handleSubmit}>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          setQuantity(Number(e.target.value));
+        }}
+        className="select"
+      >
         <option value="" disabled selected hidden>
           Qty.
         </option>
@@ -11,8 +35,18 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Add Item..." />
-      <button type="submit">Add</button>
+      <input
+        className="input"
+        type="text"
+        value={product}
+        onChange={(e) => {
+          setProduct(e.target.value);
+        }}
+        placeholder="Add Item..."
+      />
+      <button className="button" type="submit">
+        Add
+      </button>
     </form>
   );
 }
